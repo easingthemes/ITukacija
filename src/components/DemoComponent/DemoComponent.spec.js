5c059831b5c2868c7474f24c37d9910ff2905006
Dragan Filipovic <dragan.filipovic@netcentric.biz>
2018-04-08 21:27:02 +0200

    SETUP Remove Demo components

--- src/components/DemoComponent/DemoComponent.spec.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import DemoComponent from './DemoComponent';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<DemoComponent />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
--- src/containers/DemoContainer/constants.js
+++
@@ -1,2 +0,0 @@
-export const DEFAULT_ACTION = 'containers/DemoContainer/DEFAULT_ACTION';
-export const GET_DEMO = 'containers/DemoContainer/GET_DEMO';
